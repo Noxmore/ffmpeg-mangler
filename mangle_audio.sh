c431f765d89f2b7edf2b726ee9b647ccae1f61d7
Noxmore <noxmore@noxmore.dev>
2024-04-09 18:17:56 -0700

    set mangle_settings calls to use dirname

--- mangle_audio.sh
+++ mangle_audio.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 
-. ./mangle_settings.sh
+. "$(dirname "$0")/mangle_settings.sh"
 
 ffmpeg -i "$1" -b:a $MANGLE_AUDIO_BITRATE -sample_fmt s16p mangled.mp3
--- mangle_video.sh
+++ mangle_video.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 
-. ./mangle_settings.sh
+. "$(dirname "$0")/mangle_settings.sh"
 
 ffmpeg -i "$1" -b:v $MANGLE_VIDEO_BITRATE -b:a $MANGLE_AUDIO_BITRATE -vf $MANGLE_VIDEO_FILTER mangled.mp4
